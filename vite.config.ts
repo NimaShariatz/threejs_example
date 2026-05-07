ef1ce04ed66a118a166703f97ae67e12c88cd9bf
NimaShariatz <shariatz@ualberta.ca>
2026-05-07 03:50:43 -0600

    gh pages content

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  // Use the repo name for GitHub Pages build, but use '/' for local development
+  base: command === 'build' ? '/threejs_example/' : '/',
+  build: {
+    outDir: 'dist'
+  }
+}))
